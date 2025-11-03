d7f90529dfbd1591d202e6a09279cb679a83dce3
chirag moradiya <chirag.moradiya@wetechforu.com>
2025-11-03 23:46:51 +0530

    revert changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,9 +50,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* <Header /> */}
-        {children}
-        <SpeedInsights />
+        <Header />
+          {children}
+          <SpeedInsights />
         <Footer />
       </body>
     </html>
